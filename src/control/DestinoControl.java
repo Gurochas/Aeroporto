518da68ff501410e6461de2e429760a064d701bd
Gustavo <gurochasouza@gmail.com>
2020-12-01 21:07:55 -0300

    Atualizacao 01122020

--- src/control/DestinoControl.java
+++ src/control/DestinoControl.java
@@ -53,7 +53,7 @@ public class DestinoControl {
 		list = (ArrayList<Destino>) destinoDAO.buscarDestinos();
 		return list;
 	}
-	
+	//d
 	
 	public IntegerProperty getCodigoProperty() {
 		return codigo;
